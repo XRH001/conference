d35f09d8b4c119607ff50733a3cf0ec7aab74f88
NCU-xiaoxiaobai <1795490987@qq.com>
2020-12-14 20:27:31 +0800

    注册接口完毕

---
+++ src/main/java/com/example/demo/controller/RegisterYzController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.utils.MailSendUtil;
+import com.example.demo.service.mehod.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterYzController {
+    @Autowired
+    UserService userService;
+    @RequestMapping("/registerYz")
+    public String registerYz(String email) {
+        System.out.println(email);
+        // 根据邮箱查找
+        try{
+            userService.queryUserByEmail(email);
+        }catch(NullPointerException e){
+            // 如果查询不存在的邮箱将会报错 返回MD5加密后验证码给前端
+            try{
+                String emailCode = MailSendUtil.mail(email);
+                System.out.println(emailCode);
+                System.out.println(passwordCode(emailCode));
+                return passwordCode(emailCode);
+            }catch(Exception err){
+                return "error";
+            }
+        }
+        return "already";
+    }
+
+    public String passwordCode(String emailCode){
+        String str = "";
+        for(int i=0;i<emailCode.length();i++){
+            int asciiNum = (int)emailCode.charAt(i);
+            str += Integer.toHexString(asciiNum);
+        }
+        return str;
+    }
+}
