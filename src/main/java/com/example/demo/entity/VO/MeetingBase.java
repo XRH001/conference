040d6682e12de67b3568e528dc87c7534eab6657
li-jiaxv <836054533@qq.com>
2020-12-21 21:59:18 +0800

    找回丢失的VO

---
+++ src/main/java/com/example/demo/entity/VO/MeetingBase.java
@@ -0,0 +1,60 @@
+package com.example.demo.entity.VO;
+
+import com.example.demo.entity.DTO.Conference;
+
+public class MeetingBase {
+    private int id;
+    private String name;
+    private String beginTime;
+    private String endTime;
+    private String address;
+    private String remark;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(String beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
